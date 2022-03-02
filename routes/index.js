433c4ba77808f58a0fffbbb2564616a7180c82a3
Afiq Syazwan <fafiqsyaz@gmail.com>
2022-03-02 23:04:20 +0800

    changes frontend controller

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var fetchController = require('../controllers/fetchController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/fetch', fetchController.index);
+
+
 
 module.exports = router;
